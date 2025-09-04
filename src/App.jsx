d58814625a010fa65147bfc6f3a1f82e5892dc5a
Nick.Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2025-09-04 13:06:06 -0400

    Get further creating the css for badge and all of its variants.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import MainLayout from "./layouts/MainLayout/MainLayout";
 import Badge from "./components/badge/Badge";
+import { AccessibilityIcon } from "lucide-react";
 
 function App() {
   useEffect(() => {
@@ -11,8 +12,57 @@ function App() {
   return (
     <>
       <MainLayout>
-        <Badge>Badge</Badge>
-        <Badge type="pill">Badge</Badge>
+        {/* SM */}
+        <Badge size="sm">Badge</Badge>
+        <Badge size="sm" status="brand">
+          Badge
+        </Badge>
+        <Badge size="sm" status="info">
+          Badge
+        </Badge>
+        <Badge size="sm" status="success">
+          Badge
+        </Badge>
+        <Badge size="sm" status="warning">
+          Badge
+        </Badge>
+        <Badge size="sm" status="error">
+          Badge
+        </Badge>
+        {/* MD */}
+        <Badge variant="soft">Badge</Badge>
+        <Badge variant="soft" status="brand">
+          Badge
+        </Badge>
+        <Badge variant="soft" status="info">
+          Badge
+        </Badge>
+        <Badge variant="soft" status="success">
+          Badge
+        </Badge>
+        <Badge variant="soft" status="warning">
+          Badge
+        </Badge>
+        <Badge variant="soft" status="error">
+          Badge
+        </Badge>
+        {/* LG */}
+        <Badge size="lg">Badge</Badge>
+        <Badge size="lg" status="brand">
+          Badge
+        </Badge>
+        <Badge size="lg" status="info">
+          Badge
+        </Badge>
+        <Badge size="lg" status="success">
+          Badge
+        </Badge>
+        <Badge size="lg" status="warning">
+          Badge
+        </Badge>
+        <Badge size="lg" status="error">
+          Badge
+        </Badge>
       </MainLayout>
     </>
   );
--- src/components/badge/Badge.jsx
+++ src/components/badge/Badge.jsx
@@ -4,12 +4,16 @@ import cn from "../../utils/cn";
 export default function Badge({
   children,
   variant = "solid",
+  size = "md",
   status = "neutral",
   shape = "pill",
   className,
   ...rest
 }) {
-  const badgeClasses = cn(`badge ${variant} ${status} ${shape}`, className);
+  const badgeClasses = cn(
+    `badge ${variant} ${size} ${status} ${shape}`,
+    className
+  );
 
   return (
     <div className={badgeClasses} {...rest}>
