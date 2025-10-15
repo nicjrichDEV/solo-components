7eb5415320f98c38fca8fc76fcc7cbe79337e27e
Nick Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2025-10-14 21:37:27 -0400

    import sort order change.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import "./styles/reset.css";
 import "./styles/colors.css";
 import "./styles/tokens.css";
@@ -8,7 +9,6 @@ import "./styles/brands/pmi-high-contrast.css";
 import "./styles/brands/infinity.css";
 import "./styles/brands/infinity-high-contrast.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
